131ecd5ca90055f9373cab2a1735348b3430f2ab
WeissWojciech <90031814+WeissWojciech@users.noreply.github.com>
2023-10-03 21:51:23 +0200

    Update Index.js

--- zadanie_zaległe/Index.js
+++ zadanie_zaległe/Index.js
@@ -1,1 +1,26 @@
+const http = require('http');
+const fs = require('fs');
 
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+  fs.readFile('hello.html', 'utf8', (err, data) => {
+    if (err) {
+      // W przypadku błędu wyślij odpowiedź błędu
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('Błąd serwera');
+    } else {
+      // Ustaw nagłówki odpowiedzi dla pliku HTML
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      // Wyślij zawartość pliku hello.html
+      res.end(data);
+});
+
+server.listen(port, hostname, () => {
+
+  console.log(`Server is running at http://${hostname}:${port}/`);
+
+});
+  
