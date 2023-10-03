dc863f2f601f8f4a9666897afb44e5262b3265c4
WeissWojciech <90031814+WeissWojciech@users.noreply.github.com>
2023-10-03 22:03:08 +0200

    Update Index.mjs

---
+++ zadanie_zaległe/Index.mjs
@@ -0,0 +1,27 @@
+import http from 'http';
+import {readFile} from 'fs/promises';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer(async(req, res) => {
+
+  const url = req.ulr;
+  
+  if (url === '/') {
+    res.statusCode = 200;
+
+    const helo = await readFile('./hello.html');
+
+    res.setHeader('content-type', 'text/html');
+    res.write(helo);
+    res.end();
+  }
+});
+
+server.listen(port, hostname, () => {
+
+  console.log(`Server is running at http://${hostname}:${port}/`);
+
+});
+  
