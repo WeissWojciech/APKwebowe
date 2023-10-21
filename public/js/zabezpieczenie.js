38016c0ea0f11f0dd811f5a05d84d95449db0f19
WeissWojciech <90031814+WeissWojciech@users.noreply.github.com>
2023-10-21 17:15:42 +0200

    Delete public/js directory

--- public/js/zabezpieczenie.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('formularz');
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      alert('Nie można wysłać wiadomości');
-    });
-  });
